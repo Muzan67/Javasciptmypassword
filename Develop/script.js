f57c0ba30afad7df0477f351435d0e27409e2be4
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-05-08 00:11:35 -0400

    JS portion complete

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password input
 
 // We need to store the data that we need for the program
@@ -12,7 +11,6 @@ var symbol = "!@#$%^&*()"
 var finalArr = []
 var password = ''
 // console.log(numbers, lowercase, uppercase, symbol,)
-
 
 function generatePassword () {
 //get Users input
@@ -32,16 +30,24 @@ var hasUppercase = confirm("Do you want a uppercase letter");
 // if user says yes we are going to push the data of the number array into the finalArr
 //if they say no do nothing 
 
-// use a forloop to loop through final array based of the length the user requested for 
-for (let i = 0; i < lengthOfPassword; i++){
-// use a MAth.random function to get a random character from the array and store it into a password string
-finalArr.push(Math.random()* lengthOfPassword, hasLowercase, hasUppercase, hasNumber, hasSymbol);
+var arrOfSelection = []
+
+if(hasNumber) arrOfSelection.push(numbers)
+if(hasSymbol) arrOfSelection.push(symbol)
+if(hasLowercase) arrOfSelection.push(lowercase)
+if(hasUppercase) arrOfSelection.push(uppercase)
+
+console.log(arrOfSelection);
+
+// use a forloop to loop through final array based of the length the user requested for
+for (let i = 0; i < lengthOfPassword; i++) { // use a MAth.random function to get a random character from the array and store it into a password string
+var randCharSet = arrOfSelection[Math.floor(Math.random() * arrOfSelection.length)]
+  password += randCharSet.split('')[Math.floor(Math.random() * randCharSet.length)]
+  console.log(password);
 }
-return finalArr
-
-
-console.log(`length=${lengthOfPassword}  lower = ${hasLowercase}, upper = ${hasUppercase}  number = ${hasNumber}  symbol = ${hasSymbol}`);   
-}   
+   
+return password
+}
 
 // Add event listener to generate button
 function writePassword() {
